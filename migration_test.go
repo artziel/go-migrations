ef546a3f622a848d9b586f69f5077303b44072e3
Artziel Narvaiza <artziel@gmail.com>
2022-08-26 18:21:41 -0500

    add tests

--- migration_test.go
+++ migration_test.go
@@ -4,12 +4,41 @@ import (
 	"testing"
 )
 
+const testMigrationSQL = `
+-- ------------------------------------------------------------
+-- Version: 20211223120557
+-- Name: Migration Name
+-- ------------------------------------------------------------
+-- ------------------------------------------------------------
+-- Up Start
+
+INSERT INTO catalogs (id, tag) VALUES ( 1, 'MASTER' );
+
+-- Up End
+-- ------------------------------------------------------------
+-- ------------------------------------------------------------
+-- Down Start
+
+DELETE FROM catalogs;
+
+-- Down End
+-- ------------------------------------------------------------
+`
+
 type errTests struct {
 	migration       Migration
 	expectedUp      string
 	expectedDown    string
 	expectedName    string
 	expectedVersion string
+}
+
+type testBridge struct {
+	String string
+}
+
+func (t testBridge) DoSomething() string {
+	return "SomeString"
 }
 
 func TestCustomBridgeMessage(t *testing.T) {
@@ -31,5 +60,68 @@ func TestCustomBridgeMessage(t *testing.T) {
 		if test.expectedVersion != test.migration.Version {
 			t.Errorf("Test %d: Unexpected value result of parsing Migration.Version\n", i)
 		}
+	}
+}
+
+func TestParseTplSQL(t *testing.T) {
+
+	sql, err := parseTplSQL("INSERT INTO table (id, username, password)VALUES(1, 'sample@sample.com', '{{ .DoSomething }}')", testBridge{})
+	if err != nil {
+		t.Errorf("Unexpected error: %s", err)
+	} else {
+		expected := "INSERT INTO table (id, username, password)VALUES(1, 'sample@sample.com', 'SomeString')"
+		if sql != expected {
+			t.Errorf("Returned SQL dont match:\ngot  %s\nwant %s", sql, expected)
+		}
+	}
+
+	sql, err = parseTplSQL("INSERT INTO table (id, username, password)VALUES(1, 'sample@sample.com', '{{ .GenerateError }}')", testBridge{})
+	if err == nil && sql == "" {
+		t.Errorf("Expected error result got No Error")
+	}
+}
+
+func TestMigration(t *testing.T) {
+
+	m := MigrationFromString(testMigrationSQL)
+
+	expected := "Migration Name"
+	if result := m.getName(testMigrationSQL); result != expected {
+		t.Errorf("getName return unexpected value:\ngot  %s\nwant %s", result, expected)
+	}
+
+	expected = ""
+	if result := m.getName(""); result != expected {
+		t.Errorf("getName return unexpected value:\ngot  %s\nwant %s", result, expected)
+	}
+
+	expected = "20211223120557"
+	if result := m.getVersion(testMigrationSQL); result != expected {
+		t.Errorf("getVersion return unexpected value:\ngot  %s\nwant %s", result, expected)
+	}
+
+	expected = ""
+	if result := m.getVersion(""); result != expected {
+		t.Errorf("getVersion return unexpected value:\ngot  %s\nwant %s", result, expected)
+	}
+
+	expected = "INSERT INTO catalogs (id, tag) VALUES ( 1, 'MASTER' );"
+	if result := m.getUpQuery(testMigrationSQL); result != expected {
+		t.Errorf("getUpQuery return unexpected value:\ngot  %s\nwant %s", result, expected)
+	}
+
+	expected = ""
+	if result := m.getUpQuery(""); result != expected {
+		t.Errorf("getUpQuery return unexpected value:\ngot  %s\nwant %s", result, expected)
+	}
+
+	expected = "DELETE FROM catalogs;"
+	if result := m.getDownQuery(testMigrationSQL); result != expected {
+		t.Errorf("getDownQuery return unexpected value:\ngot  %s\nwant %s", result, expected)
+	}
+
+	expected = ""
+	if result := m.getDownQuery(""); result != expected {
+		t.Errorf("getDownQuery return unexpected value:\ngot  %s\nwant %s", result, expected)
 	}
 }
